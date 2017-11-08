f4f70491dc9dd60dd8958ea002fbebf7e9981415
Lenar <Leagre2010@gmail.com>
2017-11-08 17:15:09 +0300

    authentication

--- src/services/mail.go
+++ src/services/mail.go
@@ -7,12 +7,106 @@ import (
 
 	"log"
 
+	"errors"
+
+	"fmt"
+
+	"crypto/tls"
+	"encoding/base64"
+	"net"
+
 	"github.com/domodwyer/mailyak"
 	"github.com/matcornic/hermes"
 )
 
+type Authentication struct {
+	username, password string
+}
+
+func NewAuthentication(username, password string) smtp.Auth {
+	return &Authentication{username, password}
+}
+
+func (a *Authentication) Start(server *smtp.ServerInfo) (string, []byte, error) {
+	return "LOGIN", []byte{}, nil
+}
+
+func (a *Authentication) Next(fromServer []byte, more bool) ([]byte, error) {
+	if more {
+		switch string(fromServer) {
+		case "Username:":
+			return []byte(a.username), nil
+		case "Password:":
+			return []byte(a.password), nil
+		default:
+			return nil, errors.New("Unknown fromServer")
+		}
+	}
+	return nil, nil
+}
+
 type Mail interface {
 	Send(message *models.Message) error
+}
+
+func SendMail(addr string, a smtp.Auth, from string, to []string, msg []byte) error {
+	c, err := smtp.Dial(addr)
+	host, _, _ := net.SplitHostPort(addr)
+	if err != nil {
+		fmt.Println("call dial")
+		return err
+	}
+	defer c.Close()
+
+	if ok, _ := c.Extension("STARTTLS"); ok {
+		config := &tls.Config{ServerName: host, InsecureSkipVerify: true}
+		if err = c.StartTLS(config); err != nil {
+			fmt.Println("call start tls")
+			return err
+		}
+	}
+
+	if a != nil {
+		if ok, _ := c.Extension("AUTH"); ok {
+			if err = c.Auth(a); err != nil {
+				fmt.Println("check auth with err:", err)
+				return err
+			}
+		}
+	}
+
+	if err = c.Mail(from); err != nil {
+		return err
+	}
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+
+	header := make(map[string]string)
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = "text/plain; charset=\"utf-8\""
+	header["Content-Transfer-Encoding"] = "base64"
+	message := ""
+	for k, v := range header {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + base64.StdEncoding.EncodeToString(msg)
+	_, err = w.Write([]byte(message))
+
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
 }
 
 type mail struct {
@@ -20,16 +114,16 @@ type mail struct {
 }
 
 func NewMail() Mail {
-	mail := mailyak.New(
-		"smtp.gmail.com:587",
+	m := mailyak.New(
+		"mail.nefco.ru:2525",
 		smtp.PlainAuth(
 			"",
-			"leagre2010@gmail.com",
-			"GolangReact2017",
-			"smtp.gmail.com",
+			"notice@nefis.local",
+			"N76Qvb9t",
+			"mail.nefco.ru",
 		),
 	)
-	return &mail{mail}
+	return &mail{m}
 }
 
 func (c *mail) Send(msg *models.Message) error {
@@ -86,9 +180,15 @@ func (c *mail) Send(msg *models.Message) error {
 	c.mail.Subject("test")
 	c.mail.Plain().Set(mime)
 	c.mail.HTML().Set(emailBody)
+	//if err := c.mail.Send(); err != nil {
+	//	log.Panic(err)
+	//}
+	const servername, username, password = "mail.nefco.ru:2525", "notice@nefis.local", "N76Qvb9t"
+	//hostName, _, _ := net.SplitHostPort(servername)
+	auth := NewAuthentication(username, password)
 
-	if err := c.mail.Send(); err != nil {
-		log.Panic(err)
+	if err := SendMail(servername, auth, msg.From, []string{msg.To}, []byte(emailBody)); err != nil {
+		panic(err)
 	}
 
 	return nil
